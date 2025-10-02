f498da0ee3444361a88bd41563fe40dce615b55f
aliisroilov0880-sudo <aliisroilov0880@gmail.com>
2025-10-02 21:44:35 +0500

    Create hw.sql

---
+++ lesson-1/homework/hw.sql
@@ -0,0 +1,23 @@
+-- 1. Data:
+-- Data – bu xom ma’lumotlar bo‘lib, ular hali tahlil qilinmagan.
+-- Masalan: ism, yosh, manzil va h.k.
+
+-- 2. Database:
+-- Database – bu ma’lumotlarni saqlash, boshqarish va izlash uchun ishlatiladigan tizim.
+
+-- 3. Relational Database:
+-- Bu ma’lumotlar jadvallar shaklida saqlanadigan va ular o‘zaro bog‘lanadigan ma’lumotlar bazasi.
+
+-- 4. Table:
+-- Table – bu ma’lumotlarni ustunlar (column) va satrlar (row) ko‘rinishida saqlovchi strukturadir.
+
+-- 5. SQL Server’ning 5 ta asosiy funksiyasi:
+-- 1. Ma’lumotlarni xavfsiz saqlash
+-- 2. SQL orqali ma’lumotlarni so‘rash (SELECT, INSERT va h.k.)
+-- 3. Foydalanuvchi huquqlari bilan xavfsizlikni ta’minlash
+-- 4. Zaxira (backup) olish va tiklash
+-- 5. Jadval orasidagi bog‘lanishlarni o‘rnatish (kalitlar orqali)
+
+-- 6. SQL Server’dagi avtorizatsiya turlari:
+-- 1. Windows Authentication – foydalanuvchi Windows orqali tizimga kiradi.
+-- 2. SQL Server Authentication – alohida login va parol orqali kiriladi.
